baa2f037da801df41dada081d1190d3077542136
joserodrigues2019 <rodrigues.santos@gmail.com>
2021-01-13 18:30:06 -0400

    Atualização do Projeto com Information para visualizar o Nome da API e versão.

---
+++ Information/InformationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace API.Information
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class InformationController : Controller
+    {
+		public dynamic Get()
+
+		{
+			var version = Assembly.GetExecutingAssembly();
+			var result = new
+			{
+				servico = FileVersionInfo.GetVersionInfo(version.Location).ProductName,
+				versao  = FileVersionInfo.GetVersionInfo(version.Location).FileVersion	
+			};
+
+			return result;
+		}
+	}
+}
