14841b36452437224918febb10bfdd3b9c8ef419
joserodrigues2019 <rodrigues.santos@gmail.com>
2021-01-13 20:11:44 -0400

    Alteração de modelo e estrutura do projeto para API

---
+++ Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Persistence.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ContasPagarEntity> ContasPagar { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ContasPagarEntity>().ToTable("ContasPagar");
+            builder.Entity<ContasPagarEntity>().HasKey(p => p.IdConta);
+
+        }
+    }
+}
