42053866742310606da3e594a5db042a74397643
joserodrigues2019 <rodrigues.santos@gmail.com>
2021-01-13 21:05:34 -0400

    correções na Entidade ContasPagarEntity

--- Domain/Models/ContasPagarEntity.cs
+++ Domain/Models/ContasPagarEntity.cs
@@ -8,21 +8,26 @@ namespace Api.Domain.Models
     public class ContasPagarEntity
     {
         [Key]
+        [Column("id_conta")]
         public int IdConta { get; set; }
 
+        [Column("nome")]
         [StringLength(100)]
         public string Nome { get; set; }
 
+        [Column("valor_original")]
         public Decimal ValorOriginal { get; set; }
 
-        [Column("DataVencimento", TypeName = "DateTime")]
+        [Column("data_vencimento", TypeName = "DateTime")]
         public DateTime DataVencimento { get; set; }
 
-        [Column("DataPagamento", TypeName = "DateTime")]
+        [Column("data_pagamento", TypeName = "DateTime")]
         public DateTime DataPagamento { get; set; }
 
+        [Column("valor_corrigido")]
         public Decimal ValorCorrigido { get; set; }
 
+        [Column("qtde_dias_atraso")]
         public int QuantidadeDiasAtraso { get; set; }
 
     }
