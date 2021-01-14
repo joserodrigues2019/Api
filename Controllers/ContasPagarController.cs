0055feffad247ffcb2744f20f7f2568995d7a775
joserodrigues2019 <rodrigues.santos@gmail.com>
2021-01-14 08:30:44 -0400

    implementação do controllers

---
+++ Controllers/ContasPagarController.cs
@@ -0,0 +1,28 @@
+﻿using Api.Domain.Models;
+using Api.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ContasPagarController : ControllerBase
+    {
+        private readonly IContasPagarService _serviceContasPagar;
+
+        public ContasPagarController(IContasPagarService serviceContasPagar)
+        {
+            _serviceContasPagar = serviceContasPagar;
+        }
+
+        // GET: api/ContasPagarEntities
+        [HttpGet]
+        public IEnumerable<ContasPagarEntity> Get()
+        {
+            return _serviceContasPagar.Listar(); ;
+        }
+
+    }
+}
