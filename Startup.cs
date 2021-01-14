4ee0ae4712c702004976122108be7db7a886873e
joserodrigues2019 <rodrigues.santos@gmail.com>
2021-01-13 20:30:34 -0400

    Alteração do namespace para o context

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,10 @@
-using Api.DataAcess.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Api.Persistence.Contexts;
 
 namespace Api
 {
