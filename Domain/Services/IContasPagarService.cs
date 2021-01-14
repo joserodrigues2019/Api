21111e8db47aaa78d3009fe055f9edefe2484010
joserodrigues2019 <rodrigues.santos@gmail.com>
2021-01-13 20:51:05 -0400

    Implementação do serviço ContasPagar

---
+++ Domain/Services/IContasPagarService.cs
@@ -0,0 +1,12 @@
+﻿using Api.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Services
+{
+    public interface IContasPagarService
+    {
+        Task<IEnumerable<ContasPagarEntity>> ListAsync();
+        void Incluir(ContasPagarEntity contasPagar);
+    }
+}
---
+++ Services/ContasPagarService.cs
@@ -0,0 +1,28 @@
+﻿using Api.Domain.Models;
+using Api.Domain.Repositories;
+using Api.Domain.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Services
+{
+    public class ContasPagarService : IContasPagarService
+    {
+        private readonly IContasPagarRepository _contasPagarRepository;
+
+        public ContasPagarService(IContasPagarRepository contasPagarRepository)
+        {
+            _contasPagarRepository = contasPagarRepository;
+        }
+
+        public async Task<IEnumerable<ContasPagarEntity>> ListAsync()
+        {
+            return await _contasPagarRepository.ListAsync();
+        }
+
+        public async void Incluir(ContasPagarEntity contasPagar)
+        {
+            await _contasPagarRepository.Add(contasPagar);
+        }
+    }
+}
